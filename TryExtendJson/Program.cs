1ea856303fc24d4ca60b8e2e010bd111391424cb
Anthony Kwok <anthonykwok83@gmail.com>
2020-09-09 23:42:40 +0800

    init commit

---
+++ TryExtendJson/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using JsonSubTypes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+
+namespace TryExtendJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            KnownTypesBinder knownTypesBinder = new KnownTypesBinder
+            {
+                KnownTypes = new List<Type>
+                {
+                    typeof(TextBox),
+                    typeof(SignaturePad),
+                    typeof(CheckBox),
+                }
+            };
+
+            DefaultContractResolver contractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new SnakeCaseNamingStrategy()
+            };
+            
+            
+            var toSerialize = new Elements
+            {
+                new TextBox(),
+                new CheckBox(),
+                new SignaturePad(),
+            };
+
+            var settings = new JsonSerializerSettings
+            {
+                //TypeNameHandling = TypeNameHandling.Objects,
+                //SerializationBinder = knownTypesBinder,
+                ContractResolver = contractResolver,
+            };
+            
+            settings.Converters.Add(JsonSubtypesConverterBuilder
+                .Of(typeof(Element),"type")
+                .RegisterSubtype(typeof(TextBox),"text")
+                .RegisterSubtype(typeof(CheckBox),"check")
+                .RegisterSubtype(typeof(SignaturePad),"sign")
+                .SerializeDiscriminatorProperty()
+                .Build()
+            );
+            
+            var json = JsonConvert.SerializeObject(toSerialize, Formatting.Indented, settings);
+            Console.WriteLine(json);
+
+            var obj = JsonConvert.DeserializeObject<Elements>(json, settings);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TryExtendJson/Stuff.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Serialization;
+
+namespace TryExtendJson
+{
+    
+    
+    public class KnownTypesBinder : ISerializationBinder
+    {
+        public IList<Type> KnownTypes { get; set; }
+
+        public Type BindToType(string assemblyName, string typeName)
+        {
+            return KnownTypes.SingleOrDefault(t => t.Name == typeName);
+        }
+
+        public void BindToName(Type serializedType, out string assemblyName, out string typeName)
+        {
+            assemblyName = null;
+            typeName = serializedType.Name;
+        }
+    }
+    
+    public class Elements : List<Element>
+    {
+        
+    }
+    
+    public class Element
+    {
+        public string Name { get; set; } = "";
+        public double Top { get; set; } = 0;
+        public double Left { get; set; } = 0;
+        public double Width { get; set; } = 50;
+        public double Height { get; set; } = 10;
+
+    }
+
+    public class TextBox : Element
+    {
+        public string FontSize { get; set; } = "12pt";
+        public string Text { get; set; } = "";
+    }
+
+    public class SignaturePad : Element
+    {
+        public string SignatureLink { get; set; } = "";
+    }
+
+    public class CheckBox : Element
+    {
+        public bool Checked { get; set; } = false;
+    }
+}
